4d2e2e9375a87f56df6a2716e6de6ddd211c9734
Emmanuel Sackey <aximilli1212@gmail.com>
2021-11-20 21:49:40 +0000

    Add Actions and Reducers

---
+++ src/redux/actions/MenuActions.ts
@@ -0,0 +1,8 @@
+export const USER_LOGIN = 'USER_LOGIN'
+export const USER_LOGOUT = 'USER_LOGOUT'
+export function showLogin() {
+    return { type: USER_LOGIN }
+}
+export function showLogout() {
+    return { type: USER_LOGOUT }
+}
