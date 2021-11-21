b0d9874ffdb2d993726aafcfb5ee432b023ec005
Emmanuel Sackey <aximilli1212@gmail.com>
2021-11-21 10:28:29 +0000

    Add global store

--- src/redux/reducers/MenuReducer.ts
+++ src/redux/reducers/MenuReducer.ts
@@ -1,1 +1,13 @@
-
+import { USER_LOGIN, USER_LOGOUT } from '../actions/MenuActions'
+const DEFAULT_LOGIN_STATE = {
+    isLogin: false
+}
+export default (state = DEFAULT_LOGIN_STATE, action: { type: String }) => {
+    switch (action.type) {
+        case USER_LOGIN:
+            return { is_login: true }
+        case USER_LOGOUT:
+            return { is_login: false }
+        default:
+            return state
+    } }
